864fa979ed983130f29e95dd4b36b3dab7867076
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-20 12:08:01 +0000

    Updating to latest FlutterFlow output.

--- lib/authorization/authorization_widget.dart
+++ lib/authorization/authorization_widget.dart
@@ -289,8 +289,39 @@ class _AuthorizationWidgetState extends State<AuthorizationWidget> {
                                                   (_model.apiResultuyd
                                                           ?.jsonBody ??
                                                       ''),
+                                                ).toString()
+                                                ..collectionId =
+                                                    AuthorizationCall
+                                                        .recordcollectionId(
+                                                  (_model.apiResultuyd
+                                                          ?.jsonBody ??
+                                                      ''),
+                                                ).toString()
+                                                ..collectionName =
+                                                    AuthorizationCall
+                                                        .recordcollectionName(
+                                                  (_model.apiResultuyd
+                                                          ?.jsonBody ??
+                                                      ''),
+                                                ).toString()
+                                                ..username = AuthorizationCall
+                                                    .recordusername(
+                                                  (_model.apiResultuyd
+                                                          ?.jsonBody ??
+                                                      ''),
+                                                ).toString()
+                                                ..verified = AuthorizationCall
+                                                    .recordverified(
+                                                  (_model.apiResultuyd
+                                                          ?.jsonBody ??
+                                                      ''),
                                                 ).toString(),
                                             );
+                                            FFAppState().token =
+                                                AuthorizationCall.token(
+                                              (_model.apiResultuyd?.jsonBody ??
+                                                  ''),
+                                            ).toString();
                                           });
                                         } else {
                                           await showDialog(
